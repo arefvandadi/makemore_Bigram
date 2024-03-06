3be843d8e5473e907f4fbb14f8360433bd382481
arefvandadi <aref.vandadi@gmail.com>
2024-03-05 22:37:03 -0500

    Encoder and Decoder defined, book encoded and converted to tensor

--- BigramModel_with_SelfAttention.py
+++ BigramModel_with_SelfAttention.py
@@ -9,6 +9,16 @@ with open('input.txt', 'r', encoding='utf-8') as tinyshkspr:
 
 r = sorted(set(book))
 chars = ''.join(r)
-# Number of unique characters in the model
+# Number of unique characters in the model (charsize = 65)
 charsize = len(chars)
+
+## I used rfind() function to create my encoder and deccoder instead of
+## using dictionary which was used in the Original code
+encoder = lambda c: [chars.rfind(c[i]) for i in range(len(c))]
+decoder = lambda c: "".join([chars[i] for i in c])
+
+# Encode the Book (Type = Python List)
+book_digits_List=encoder(book)
+# Convert Encoded Book from python List to PyTorch Tensor
+book_digits = torch.tensor(book_digits_List)
 
