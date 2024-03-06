1ff42284014a44e86de3ec6e2a2ec806caf3119e
arefvandadi <aref.vandadi@gmail.com>
2024-03-05 21:59:49 -0500

    Added code to read the Tiny Shakespeare (input.txt) in

--- BigramModel_with_SelfAttention.py
+++ BigramModel_with_SelfAttention.py
@@ -2,3 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Let's download and read in the tiny shakespeare dataset
+#!wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as tinyshkspr:
+    book = tinyshkspr.read()
+
+
