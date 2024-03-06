e7f8bb81565b3253f7e9c8be2b990ef1fa05ae1b
arefvandadi <aref.vandadi@gmail.com>
2024-03-05 22:47:27 -0500

    All required hyperparameters were added

--- BigramModel_with_SelfAttention.py
+++ BigramModel_with_SelfAttention.py
@@ -2,6 +2,54 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+################ Initial Hyperparameters ###########
+## Hyperparameter ##
+#number of embedding for each character --> i.e. C channels entering Self-Attention Head
+n_embed = 32
+# number of batches for each iteration
+batch_size = 4
+# How many characters are used to predict the next? Context = 1 --> Bigram model, Context > 1 --> Ngram model
+context = 8
+# number of Self-Attention heads
+num_heads = 4
+# size of Self-Attention head
+head_size = n_embed//num_heads
+# number of Transformer (Multi-head Self-Attention + Feed Forward) layers
+n_layer = 3
+# dropout
+dropout = 0.2
+# Max Number of Iteration to Train the Model
+max_iters = 5000
+# Loss Evaluation Interval
+eval_interval = 100
+# Number of batches to Mean over for Loss evaluation at every interval
+eval_iters = 200
+################ Initial Hyperparameters ###########
+
+################ Initial Hyperparameters Multiplied by 4 ###########
+# ## Hyperparameter ##
+# #number of embedding for each character --> i.e. C channels entering Self-Attention Head
+# n_embed = 128
+# # number of batches for each iteration
+# batch_size = 16
+# # How many characters are used to predict the next? Context = 1 --> Bigram model, Context > 1 --> Ngram model
+# context = 32
+# # number of Self-Attention heads
+# num_heads = 16
+# # size of Self-Attention head
+# head_size = n_embed//num_heads
+# # number of Transformer (Multi-head Self-Attention + Feed Forward) layers
+# n_layer = 12
+# # dropout
+# dropout = 0.2
+# # Max Number of Iteration to Train the Model
+# max_iters = 5000
+# # Loss Evaluation Interval
+# eval_interval = 100
+# # Number of batches to Mean over for Loss evaluation at every interval
+# eval_iters = 200
+################ Initial Hyperparameters Multiplied by 2 ###########
+
 # Let's download and read in the tiny shakespeare dataset
 #!wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as tinyshkspr:
